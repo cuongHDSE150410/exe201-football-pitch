411e56629bb6112bab04a5ae34ed6e5105e9339d
cuonghd280201 <cuongfpt2001>
2023-08-02 15:59:57 +0700

    commit yard service

--- src/main/java/com/exe201/backend/api/v1/yard/search/SearchYardRestApi.java
+++ src/main/java/com/exe201/backend/api/v1/yard/search/SearchYardRestApi.java
@@ -4,6 +4,7 @@ package com.exe201.backend.api.v1.yard.search;
 import com.exe201.backend.service.YardService;
 import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +21,26 @@ public class SearchYardRestApi {
 
     @PostMapping(value = "search")
     public ResponseEntity<String> searchYardByLocation(@RequestBody(required = false) SearchYardRequest searchYardRequest) {
-        if (searchYardRequest == null) {
-            YardResponse yardResponse = yardService.findYardByFilter(null, null, null, null);
-            return ResponseEntity.ok().body(gson.toJson(yardResponse));
-        }
+//        if (searchYardRequest == null) {
+//            YardResponse yardResponse = yardService.findYardByFilter(null, null, null, null);
+//            return ResponseEntity.ok().body(gson.toJson(yardResponse));
+//        }
+//
+//        YardResponse yardResponse = yardService.findYardByFilter(searchYardRequest.getProvinceId(), searchYardRequest.getDistrictId(), searchYardRequest.getItemsPerPage(), searchYardRequest.getPage());
+//        return ResponseEntity.ok().body(gson.toJson(yardResponse));
+//    }
+        try {
+            if (searchYardRequest == null) {
+                YardResponse yardResponse = yardService.findYardByFilter(null, null, null, null);
+                return ResponseEntity.ok().body(gson.toJson(yardResponse));
+            }
 
-        YardResponse yardResponse = yardService.findYardByFilter(searchYardRequest.getProvinceId(), searchYardRequest.getDistrictId(), searchYardRequest.getItemsPerPage(), searchYardRequest.getPage());
-        return ResponseEntity.ok().body(gson.toJson(yardResponse));
-    }
-}
+            YardResponse yardResponse = yardService.findYardByFilter(searchYardRequest.getProvinceId(), searchYardRequest.getDistrictId(), searchYardRequest.getItemsPerPage(), searchYardRequest.getPage());
+            return ResponseEntity.ok().body(gson.toJson(yardResponse));
+        } catch (Exception e) {
+            // Handle any exceptions that may occur during execution
+            e.printStackTrace();
+            String errorMessage = "An error occurred while processing the request.";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+}}
