ee0b96f5924e6ae37d70b38c8aefca06c0f99a33
cuonghd280201 <cuongfpt2001>
2023-08-09 01:12:24 +0700

    commit column reference

--- src/main/java/com/exe201/backend/api/v1/slot/GetSlotRequest.java
+++ src/main/java/com/exe201/backend/api/v1/slot/GetSlotRequest.java
@@ -1,19 +1,22 @@
 package com.exe201.backend.api.v1.slot;
 
 import com.exe201.backend.utils.DateHelper;
+import com.google.type.DateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.sql.Timestamp;
 
 @Data
-@AllArgsConstructor
-//@NoArgsConstructor
+//@AllArgsConstructor
+@NoArgsConstructor
 public class GetSlotRequest {
-    @NonNull
-    private String date;
+    @NotNull
+    private DateTime date;
 
 
 }
